4abf5bf219bd90e1242917898b978139f3db2dd9
chihaamin <amin.chiha@gmail.com>
2025-09-25 21:43:09 +0100

    Add 4-print_hexa.py to print numbers in decimal and hexadecimal format

---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_hexa():
+    for num in range(0, 99):
+        print("{:02d} = 0x{:x}".format(num, num))
+    print("")  # Print a newline at the end
+
+
+if __name__ == "__main__":
+    print_hexa()
