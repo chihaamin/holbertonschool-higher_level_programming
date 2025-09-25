d7a914edc6681a673cd126759deeb84b7d33800e
chihaamin <amin.chiha@gmail.com>
2025-09-25 21:29:04 +0100

    fix 0-positive_or_negative python style

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
+
+
 def positive_or_negative(i):
     """Prints whether a number is positive or negative.
     Args:
@@ -15,4 +16,6 @@ def positive_or_negative(i):
     else:
         print(f"{i} is positive")
     return
+
+
 positive_or_negative(number)
