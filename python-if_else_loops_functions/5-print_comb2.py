0ae81ee1f2ff3d0c6f478e22f7e24ea782f70cd2
chihaamin <amin.chiha@gmail.com>
2025-09-25 21:51:44 +0100

    removing whitespaces

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -7,7 +7,7 @@ def comb2():
             print("{:02}".format(number))
         else:
             print("{:02}".format(number), end=", ")
-                
+
 
 if __name__ == "__main__":
     comb2()
