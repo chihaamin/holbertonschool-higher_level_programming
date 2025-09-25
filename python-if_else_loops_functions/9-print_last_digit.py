03d46ccf8d2844e217a973702b24c7d1a03cc5d0
chihaamin <amin.chiha@gmail.com>
2025-09-25 22:02:05 +0100

    Refactor print_last_digit function to simplify last digit retrieval and printing

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,6 @@
 
 
 def print_last_digit(number):
-    if number < 0:
-        number = -number
-    last_digit = number % 10
-    return last_digit
+    """Print the last digit of a number and return it."""
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
