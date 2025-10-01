136a92997ce00ce89785e4fdc8c1d0447d0d0ed1
chihaamin <amin.chiha@gmail.com>
2025-10-01 04:58:57 +0100

    refactor

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""Write a script that adds all arguments to a Python list"""
+
 import sys
+import os
+import json
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+filename = "add_item.json"
+my_list = []
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, filename)
